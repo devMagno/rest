dfd7cedfc091edbf43f48f6ac915c8bec21ed9f5
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-02-05 10:09:29 -0300

    Adiciona configuração do menu

--- app/public/wp-content/themes/rest/functions.php
+++ app/public/wp-content/themes/rest/functions.php
@@ -6,5 +6,10 @@ remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 remove_action('wp_head', 'feed_links_extra', 3 );
 remove_action('wp_head', 'wp_generator' );
 
+add_theme_support('menus');
+function register_my_menu() {
+  register_nav_menu('header-menu',__( 'Menu Principal' ));
+}
+add_action( 'init', 'register_my_menu' );
 
 ?>
